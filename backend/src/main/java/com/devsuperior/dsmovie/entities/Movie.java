07bb96e45119afc6e3bbf7ca09ecb3a30ce72629
misaelorelio <misaelorelio@hotmail.com>
2022-03-09 14:35:20 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
@@ -0,0 +1,75 @@
+package com.devsuperior.dsmovie.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_movie")
+public class Movie {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private Double score;
+	private Integer count;
+	private String image;
+	
+	public Movie () {
+		
+	}
+
+	public Movie(Long ind, String title, Double score, Integer count, String image) {
+		this.id = ind;
+		this.title = title;
+		this.score = score;
+		this.count = count;
+		this.image = image;
+	}
+
+	public Long getInd() {
+		return id;
+	}
+
+	public void setInd(Long ind) {
+		this.id = ind;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
+	}
+
+	public Integer getCount() {
+		return count;
+	}
+
+	public void setCount(Integer count) {
+		this.count = count;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+	
+	
+}
